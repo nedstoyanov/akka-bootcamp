cf124f4e937f4cc95c53bec3da77e38ab859d8a7
Ned Stoyanov <ned_77@iinet.net.au>
2016-05-05 18:31:56 +1000

    Lesson 2

---
+++ src/Unit-1/DoThis/Messages.cs
@@ -0,0 +1,55 @@
+﻿//----------------------------------------------------------------------------
+//  Copyright (c) 2016 Schneider Electric (Australia) Pty Ltd.
+//  All rights reserved. 
+//----------------------------------------------------------------------------
+
+namespace WinTail
+{
+    public class Messages
+    {
+        // in Messages.cs
+        #region Neutral/system messages
+
+        public class ContinueProcessing { }
+
+        #endregion
+
+        #region Success messages
+
+        public class InputSuccess
+        {
+            public InputSuccess(string reason)
+            {
+                Reason = reason;
+            }
+
+            public string Reason { get; private set; }
+        }
+
+        #endregion
+
+        #region Error messages
+
+        public class InputError
+        {
+            public InputError(string reason)
+            {
+                Reason = reason;
+            }
+
+            public string Reason { get; private set; }
+        }
+
+        public class NullInputError : InputError
+        {
+            public NullInputError(string reason) : base(reason) { }
+        }
+
+        public class ValidationError : InputError
+        {
+            public ValidationError(string reason) : base(reason) { }
+        }
+
+        #endregion
+    }
+}
